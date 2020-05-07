8b09e548b8e6bb6d30f1f23819519b0b1c935186
hso0321 <hso0321@naver.com>
2020-05-07 10:21:20 +0900

    주석 추가

--- publisher.py
+++ publisher.py
@@ -1,5 +1,7 @@
-#!/usr/bin/env python
-import rospy
+# !/usr/bin/env python   
+# 윗줄은 shebang line 이 프로그램이 실행될 파이썬 인터프리터를 가리킴
+# env 세팅 혹은 venv을 이용하면 한기기 여러환경에서 여러 프로그램을 실행 가능할듯
+import rospy    # 파이썬 기반
 from std_msgs.msg import Int32
 
 rospy.init_node('topic_punlisher')
