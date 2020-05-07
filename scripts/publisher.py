ab5b60721968f8c6b4749310a7a5fb2e2408d189
hso0321 <hso0321@naver.com>
2020-05-07 17:45:52 +0900

    주석추가

--- scripts/publisher.py
+++ scripts/publisher.py
@@ -2,6 +2,7 @@
 #-*- coding:utf-8 -*-
 # 윗줄은 shebang line 이 프로그램이 실행될 파이썬 인터프리터를 가리킴
 # env 세팅 혹은 venv을 이용하면 한기기 여러환경에서 여러 프로그램을 실행 가능할듯
+# 파이썬 2 쓰도록 바꾸고 인코딩 utf-8로 변경
 
 import rospy    # 파이썬 기반
 from std_msgs.msg import Int32
