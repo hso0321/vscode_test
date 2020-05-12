f3c88d20fcac3d7ab8e0631d64de677a3921d990
hso0321 <hso0321@naver.com>
2020-05-12 15:41:48 +0900

    오류 수정

--- scripts/image_process_test.py
+++ scripts/image_process_test.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python2
+#-*- coding:utf-8 -*-
+
 
 import rospy                      # rospy
 import numpy as np                # numpy
@@ -56,10 +58,9 @@ def mini_histo(gray, level):
     result = []
     roi_length = 50
     last_level = 30
-
     for x in range(w):
         sum = 0
-        for y in range(h - last_level - roi_length, h - last_level):
+        for y in range(h - last_level- roi_length, h - last_level):
             if gray[y, x] < level:
                 sum += 1
         result.append(sum)
@@ -90,48 +91,48 @@ def find_angle(gray):
     lines = hough_line(sobel_X)
 
     if lines is not None:
-            print('lines =', len(lines))
             angle_list = []
             for line in lines:
                 [[x1, y1, x2, y2]] = line
-                cv2.line(warped, (x1, y1), (x2, y2), (0, 0, 255), 3)
+                # cv2.line(warped, (x1, y1), (x2, y2), (0, 0, 255), 3)
                 if (x1 - x2) is not 0:
                     angle = np.arctan((y2 - y1) / (x2 - x1))
                 else:
                     angle = np.pi/2
-                if angle > 0:
+                if angle > 0 and (np.pi - angle) > 0.1:
                     angle_list.append(angle)
                 else:
-                    angle += np.pi
-                    angle_list.append(angle)
+                    pass
             sum = np.sum(angle_list, axis=0)
+            print(angle_list)
             average_angle = sum / len(angle_list)
-            try:
-                dx = math.cos(average_angle)
-                try:
-                    dy = math.sin(average_angle)
-                    cv2.line(warped, (160, 240), (int(160 - 30 * dx), int(240 - 30 * dy)), (255, 0, 0))
-                except:
-                    print('error in this angle to sin =', average_angle)
-            except:
-                print('error in this angle to cos =', average_angle)
+            # try:
+            #     dx = math.cos(average_angle)
+            #     try:
+            #         dy = math.sin(average_angle)
+            #         cv2.line(warped, (160, 240), (int(160 - 30 * dx), int(240 - 30 * dy)), (255, 0, 0))
+            #     except:
+            #         print('error in this angle to sin =', average_angle)
+            # except:
+            #     print('error in this angle to cos =', average_angle)
     return average_angle
 
 
 def image_callback(msg):
-    print("PyImageSubscriber node  Received an image!")
+    # print("PyImageSubscriber node  Received an image!")
     try:
         # Convert your ROS Image message to OpenCV2
         cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
     except CvBridgeError, e:
         print(e)
     else:
-        b_img = cv2.cvtColor(cv2_img,cv2.IMREAD_GRAYSCALE)
+        b_img = cv2.cvtColor(cv2_img,cv2.COLOR_BGR2GRAY)
+        
         # Display the converted image
         # cv2.imshow("Image Display", cv2_img)
         average_angle = find_angle(b_img)
         print("Average_angle =", average_angle)
-        cv2.imshow("Gray Image Display", b_img)
+        #cv2.imshow("Gray Image Display", b_img)
 
         # road_angle = find_angle(b_img)
 
@@ -147,7 +148,7 @@ def image_listener():
     rospy.init_node('py_image_listener')
     # Setupt the subscription, camera/rb/image_raw is used in turtlebot_gazebo example
     #rospy.Subscriber("jetbot_camera/raw", Image, image_callback)
-    rospy.Subscriber("/videofile/image_raw", Image, image_callback)
+    rospy.Subscriber("/video_publisher/image_raw_bgr8", Image, image_callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
--- scripts/test_image_publisher.py
+++ scripts/test_image_publisher.py
@@ -3,12 +3,9 @@
 # 윗줄은 shebang line 이 프로그램이 실행될 파이썬 인터프리터를 가리킴
 # env 세팅 혹은 venv을 이용하면 한기기 여러환경에서 여러 프로그램을 인터프리터 사용 가능할듯
 # 파이썬 2 쓰도록 바꾸고 인코딩 utf-8로 변경
-
 import rospy    # 파이썬 기반
 import sys
 from sensor_msgs.msg import Image
-
-class image
 
 rospy.init_node('test_image_publisher')
 
