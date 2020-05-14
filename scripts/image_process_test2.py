dd42b232322c3a2d0f551655e491de3d1ac4df45
hso0321 <hso0321@naver.com>
2020-05-14 16:50:27 +0900

    빈칸 정리

--- scripts/image_process_test2.py
+++ scripts/image_process_test2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python2
 #-*- coding:utf-8 -*-
 
-
 import rospy                      # rospy
 import numpy as np                # numpy
 import cv2                        # OpenCV2
@@ -11,7 +10,6 @@ import math
 
 #Instantiate CV Bridge
 bridge = CvBridge()
-
 
 class Line:
     def __init__(self):
@@ -37,6 +35,105 @@ class Line:
         self.road_inf = None
         self.curvature = None
         self.deviation = None
+
+left_line = Line()
+right_line = Line()
+
+th_sobelx, th_sobely, th_mag, th_dir = (35, 100), (30, 255), (30, 255), (0.7, 1.3)
+th_h, th_l, th_s = (10, 100), (0, 60), (85, 255)
+
+
+def undistort(img, mtx, dist):
+    """ undistort image """
+    return cv2.undistort(img, mtx, dist, None, mtx)
+
+
+def sobel_xy(img, orient='x', thresh=(20, 100)):
+    """
+    Define a function that applies Sobel x or y.
+    The gradient in the x-direction emphasizes edges closer to vertical.
+    The gradient in the y-direction emphasizes edges closer to horizontal.
+    """
+    # img = exposure.equalize_hist(img)
+    # adaptive histogram equalization
+    # img = exposure.equalize_adapthist(img, clip_limit=0.01)
+
+    if orient == 'x':
+        abs_sobel = np.absolute(cv2.Sobel(img, cv2.CV_64F, 1, 0))
+    if orient == 'y':
+        abs_sobel = np.absolute(cv2.Sobel(img, cv2.CV_64F, 0, 1))
+    # Rescale back to 8 bit integer
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    binary_output = np.zeros_like(scaled_sobel)
+    binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 255
+
+    # Return the result
+    return binary_output
+
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    """
+    Define a function to return the magnitude of the gradient
+    for a given sobel kernel size and threshold values
+    """
+
+    # adaptive histogram equalization
+    # img = exposure.equalize_adapthist(img, clip_limit=0.01)
+
+    # Take both Sobel x and y gradients
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Calculate the gradient magnitude
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    # Rescale to 8 bit
+    scale_factor = np.max(gradmag)/255
+    gradmag = (gradmag/scale_factor).astype(np.uint8)
+    # Create a binary image of ones where threshold is met, zeros otherwise
+    binary_output = np.zeros_like(gradmag)
+    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 255
+
+    # Return the binary image
+    return binary_output
+
+def dir_thresh(img, sobel_kernel=3, thresh=(0.7, 1.3)):
+    """
+    computes the direction of the gradient
+    """
+    # Calculate the x and y gradients
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Take the absolute value of the gradient direction,
+    # apply a threshold, and create a binary image result
+    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+    binary_output = np.zeros_like(absgraddir)
+    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 255
+    # Return the binary image
+    return binary_output.astype(np.uint8)
+
+
+
+def gradient_combine(img, th_x, th_y, th_mag, th_dir):
+    """
+    Find lane lines with gradient information of Red channel
+    """
+    rows, cols = img.shape[:2]
+    R = img[220:rows - 12, 0:cols, 2]
+
+    sobelx = sobel_xy(R, 'x', th_x)
+    #cv2.imshow('sobel_x', sobelx)
+    sobely = sobel_xy(R, 'y', th_y)
+    #cv2.imshow('sobel_y', sobely)
+    mag_img = mag_thresh(R, 3, th_mag)
+    #cv2.imshow('sobel_mag', mag_img)
+    dir_img = dir_thresh(R, 15, th_dir)
+    #cv2.imshow('result5', dir_img)
+
+    # combine gradient measurements
+    gradient_comb = np.zeros_like(dir_img).astype(np.uint8)
+    gradient_comb[((sobelx > 1) & (mag_img > 1) & (dir_img > 1)) | ((sobelx > 1) & (sobely > 1))] = 255
+
+    return gradient_comb
+
+
 
 def warp_image(img, src, dst, size):
     """ Perspective Transform """
@@ -554,7 +651,7 @@ def print_road_map(image, left_line, right_line):
     road_map = cv2.cvtColor(road_map, cv2.COLOR_BGRA2BGR)
     return road_map
 
-    def comb_result(grad, hls):
+def comb_result(grad, hls):
     """ give different value to distinguish them """
     result = np.zeros_like(hls).astype(np.uint8)
     #result[((grad > 1) | (hls > 1))] = 255
@@ -563,11 +660,42 @@ def print_road_map(image, left_line, right_line):
 
     return result
 
+def ch_thresh(ch, thresh=(80, 255)):
+    binary = np.zeros_like(ch)
+    binary[(ch > thresh[0]) & (ch <= thresh[1])] = 255
+    return binary
+
+
+def hls_combine(img, th_h, th_l, th_s):
+    # convert to hls color space
+    hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+
+    rows, cols = img.shape[:2]
+    R = img[220:rows - 12, 0:cols, 2]
+    _, R = cv2.threshold(R, 180, 255, cv2.THRESH_BINARY)
+    #cv2.imshow('red!!!',R)
+    H = hls[220:rows - 12, 0:cols, 0]
+    L = hls[220:rows - 12, 0:cols, 1]
+    S = hls[220:rows - 12, 0:cols, 2]
+
+    h_img = ch_thresh(H, th_h)
+    #cv2.imshow('HLS (H) threshold', h_img)
+    l_img = ch_thresh(L, th_l)
+    #cv2.imshow('HLS (L) threshold', l_img)
+    s_img = ch_thresh(S, th_s)
+    #cv2.imshow('HLS (S) threshold', s_img)
+
+    # Two cases - lane lines in shadow or not
+    hls_comb = np.zeros_like(s_img).astype(np.uint8)
+    hls_comb[((s_img > 1) & (l_img == 0)) | ((s_img == 0) & (h_img > 1) & (l_img > 1))] = 255 # | (R > 1)] = 255
+    #hls_comb[((s_img > 1) & (h_img > 1)) | (R > 1)] = 255
+    return hls_comb
+
 
 
 """ ros와 line detect 구문 분리용 주석"""
 
-    def image_callback(msg):
+def image_callback(msg):
     # print("PyImageSubscriber node  Received an image!")
     try:
         # Convert your ROS Image message to OpenCV2
@@ -575,7 +703,7 @@ def print_road_map(image, left_line, right_line):
     except CvBridgeError, e:
         print(e)
     else:
-        undist_img = undistort(cv2_img, mtx, dist)
+        undist_img = undistort(cv2_img, mat, dist)
         # resize video
         undist_img = cv2.resize(undist_img, None, fx=1 / 2, fy=1 / 2, interpolation=cv2.INTER_AREA)
         rows, cols = undist_img.shape[:2]
